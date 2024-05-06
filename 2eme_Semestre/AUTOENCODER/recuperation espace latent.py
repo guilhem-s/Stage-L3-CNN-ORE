f9441c231e549dd272369285c537ea104f9f8859
guilhem-s <guilhemsimeao@gmail.com>
2024-05-07 01:20:59 +0200

    Changement de cap

---
+++ 2eme_Semestre/AUTOENCODER/recuperation espace latent.py
@@ -0,0 +1,6 @@
+
+
+
+# Note : là où il y a des commentaires "pour Théo uniquement si assez de temps disponible", c'est pour faire un autoencodeur hybride, donc un réseau qui sache à la fois reconstruire son entrée via le décodeur, et sache faire une autre tâche en parrallèle, comme la tâche d'identification par exemple. Ici, j'ai fait une branche de type classifieur mais si on veut faire une tâche d'identification il faudra changer cela par autre chose (sortie à vecteurs aléatoires ou softmax à 500 neurones si on applique ce réseau sur VGG). 
+
+
